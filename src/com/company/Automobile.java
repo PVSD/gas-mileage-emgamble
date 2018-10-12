6787895cf6b4b463ff85d827e80ad298964f22e6
emgamble <eg913@pvsd.org>
2018-10-11 21:18:25 -0400

    It returns -4 which isn't right??

--- src/com/company/Automobile.java
+++ src/com/company/Automobile.java
@@ -4,4 +4,29 @@ package com.company;
  * Created by dpennebacker on 10/9/16.
  */
 public class Automobile {
+
+    public int MilesPerGallon;
+
+    public Automobile(int MPG){
+        MilesPerGallon = MPG;
+    }
+
+    public int gallons;
+
+    public void fillUp (int g){
+        g = gallons;
+    }
+
+    public int miles;
+    double leftoverFuel;
+    public void takeTrip(int m){
+        miles = m;
+
+        leftoverFuel = (gallons - (miles/MilesPerGallon));
+    }
+    public double reportFuel(){
+        return leftoverFuel;
+    }
+
+
 }
